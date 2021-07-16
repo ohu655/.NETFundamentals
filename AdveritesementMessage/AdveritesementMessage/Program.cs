f9130593edbecd006d7dd0eea136742902631e90
Martin Petrov <68014327+ohu655@users.noreply.github.com>
2021-07-16 22:09:17 +0300

    Add files via upload

---
+++ AdveritesementMessage/AdveritesementMessage/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Linq;
+
+namespace AdveritesementMessage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] phrases = new string[]
+            {
+                "Excellent product.",
+                "Such a great product.",
+                "I always use that product.",
+                "Best product of its category.",
+                "Exceptional product.",
+                "I can’t live without this product."
+            };
+
+            string[] events = new string[]
+            {
+                "Now I feel good.",
+                "I have succeeded with this product.",
+                "Makes miracles. I am happy of the results!",
+                "I cannot believe but now I feel awesome.",
+                "Try it yourself, I am very satisfied.",
+                "I feel great!"
+            };
+
+            string[] authors = new string[]
+            {
+                "Diana",
+                "Petya",
+                "Stella",
+                "Elena",
+                "Katya",
+                "Iva",
+                "Annie",
+                "Eva"
+            };
+
+            string[] cities = new string[] { "Burgas", "Sofia", "Plovdiv", "Varna", "Ruse" };
+
+            int n = int.Parse(Console.ReadLine());
+
+            Random randon = new Random();
+
+            for (int i = 0; i < n; i++)
+            {
+                int phraseIdx = randon.Next(0, phrases.Length);
+                int eventsIdx = randon.Next(0, events.Length);
+                int authorsIdx = randon.Next(0, authors.Length);
+                int citiesIdx = randon.Next(0, cities.Length);
+
+                Console.WriteLine($"{phrases[phraseIdx]} {events[eventsIdx]} {authors[authorsIdx]} – {cities[citiesIdx]}.");
+            }
+
+        }
+    }
+}
---
+++ AppendArrays/AppendArrays/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AppendArrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> input = Console.ReadLine()
+                .Split()
+                .ToList();
+
+
+            while (true)
+            {
+                string line = Console.ReadLine();
+
+                if (line == "3:1")
+                {
+                    break;
+                }
+
+                string[] parts = line.Split();
+
+                string command = parts[0];
+
+                if (command == "merge")
+                {
+                    int startIndex = int.Parse(parts[1]);
+                    int endIndex = int.Parse(parts[2]);
+
+                    if (startIndex >= input.Count)
+                    {
+                        continue;
+                    }
+
+                    if (endIndex < 0)
+                    {
+                        continue;
+                    }
+                    if (startIndex < 0)
+                    {
+                        startIndex = 0;
+                    }
+
+                    if (endIndex >= input.Count)
+                    {
+                        endIndex = input.Count -1;
+                    }
+
+                    string merged = "";
+
+                    for (int i = startIndex; i <= endIndex ; i++)
+                    {
+                        string currentElement = input[i];
+
+                        merged += currentElement;
+                    }
+
+                    input.RemoveRange(startIndex, endIndex - startIndex + 1);
+                    input.Insert(startIndex, merged);
+                }
+
+                else if (command == "divide")
+                {
+                    int index = int.Parse(parts[1]);
+                    int partitions = int.Parse(parts[2]);
+
+                    string element = input[index];
+
+                    input.RemoveAt(index);
+
+                    int partitionSize = element.Length / partitions;
+
+                    List<string> substrings = new List<string>();
+
+                    for (int i = 0; i < partitions - 1; i++)
+                    {
+                        string substring = element.Substring(i * partitionSize, partitionSize);
+
+                        substrings.Add(substring);
+                    }
+
+                    string lastSubstring = element.Substring((partitions - 1) * partitionSize);
+                    substrings.Add(lastSubstring);
+
+                    input.InsertRange(index, substrings);
+                }
+            }
+
+            Console.WriteLine(string.Join(" ", input));
+        }
+    }
+}

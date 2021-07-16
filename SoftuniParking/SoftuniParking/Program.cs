045008857c509a1fbd0c899b28e6f1e5f1efff79
Martin Petrov <68014327+ohu655@users.noreply.github.com>
2021-07-16 23:04:28 +0300

    Add files via upload

---
+++ SoftuniParking/SoftuniParking/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SoftuniParking
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> registerPlates = new Dictionary<string, string>();
+
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] line = Console.ReadLine().Split();
+                string command = line[0];
+
+                if (command == "register")
+                {
+                    string username = line[1];
+                    string plateNumber = line[2];
+
+                    if (registerPlates.ContainsKey(username))
+                    {
+                        Console.WriteLine($"ERROR: already registered with plate number {plateNumber}");
+                    }
+                    else
+                    {
+                        registerPlates.Add(username, plateNumber);
+                        Console.WriteLine($"{username} registered {plateNumber} successfully");
+                    }
+                }
+                else
+                {
+                    string username = line[1];
+
+                    if (!registerPlates.ContainsKey(username))
+                    {
+                        Console.WriteLine($"ERROR: user {username} not found");
+                    }
+                    else
+                    {
+                        registerPlates.Remove(username);
+                        Console.WriteLine($"{username} unregistered successfully");
+                    }
+                }
+            }
+
+            foreach (var kvp in registerPlates)
+            {
+                Console.WriteLine($"{kvp.Key} => {kvp.Value}");
+            }
+        }
+    }
+}
---
+++ StoreBoxes/StoreBoxes/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace StoreBoxes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Box> products = new List<Box>();
+
+            while (true)
+            {
+                string line = Console.ReadLine();
+
+                if (line == "end")
+                {
+                    break;
+                }
+
+                string[] parts = line.Split();
+                int serialNumber = int.Parse(parts[0]);
+                string itemName = parts[1];
+                int itemQuantity = int.Parse(parts[2]);
+                decimal itemPrice = decimal.Parse(parts[3]);
+                decimal priceForBox = itemPrice * itemQuantity;
+
+                Box box = new Box();
+
+                box.SerialNumber = serialNumber;
+                box.Item.Name = itemName;
+                box.ItemQuantity = itemQuantity;
+                box.Item.Price = itemPrice;
+                box.PriceForBox = priceForBox;
+                
+                products.Add(box);
+                
+            }
+
+
+            List<Box> sortedBox = products.OrderBy(boxes => boxes.PriceForBox).ToList();
+            sortedBox.Reverse();
+
+            foreach (Box box in sortedBox)
+            {
+                
+                Console.WriteLine($"{box.SerialNumber}");
+                Console.WriteLine($"-- {box.Item.Name} - ${box.Item.Price:f2}: {box.ItemQuantity}");
+                Console.WriteLine($"-- ${box.PriceForBox:f2}");
+            }
+        }
+    }
+
+    class Item
+    {
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+    }
+
+    class Box
+    {
+        public Box()
+        {
+            Item = new Item();
+        }
+
+        public int SerialNumber { get; set; }
+        public Item Item { get; set; }
+        public int ItemQuantity { get; set; }
+        public decimal PriceForBox { get; set; }
+    }
+}

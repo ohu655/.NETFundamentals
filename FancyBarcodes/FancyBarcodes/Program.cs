278d7bf7ed8423d3e2bc771a77b1e6dd6a68715c
Martin Petrov <68014327+ohu655@users.noreply.github.com>
2021-07-20 23:21:10 +0300

    Add files via upload

---
+++ FancyBarcodes/FancyBarcodes/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace FancyBarcodes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Regex barcodePattern = new Regex(@"@#+([A-Z][A-Za-z0-9]{4,}[A-Z])@#+");
+            Regex numbersPattern = new Regex(@"(?<numbers>\d+)");
+
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+               string barcode = Console.ReadLine();
+
+                Match match = barcodePattern.Match(barcode);
+
+                if (!match.Success)
+                {
+                    Console.WriteLine("Invalid barcode");
+                }
+                else
+                {
+                
+                    MatchCollection matchNumber = numbersPattern.Matches(barcode);
+
+                    if (matchNumber.Count < 1)
+                    {
+                        Console.WriteLine("Product group: 00");
+                    }
+                    else
+                    {
+                        string number = GetNumber(matchNumber);
+
+                        Console.WriteLine($"Product group: {number}");
+                    }
+                   
+                    
+                }
+            }
+        }
+
+        private static string GetNumber(MatchCollection matchCollection)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            foreach (Match match in matchCollection)
+            {
+                sb.Append(match);
+            }
+
+            return sb.ToString();
+        }
+    }
+}
